45b7adf55d8aec8a6fa38d4a2f3719ec05fe1e5e
AlirezaBVB <72646765+AlirezaBVB@users.noreply.github.com>
2020-12-11 14:39:33 +0330

    Fixed Nav

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -33,3 +33,9 @@ $(document).ready(function(){
       } // End if
     });
   });
+  $(document).ready(function(){
+    //nav toggle
+    $(".navToggle").click(function(){
+      $(".header .nav").slideToggle()
+    })
+  })
